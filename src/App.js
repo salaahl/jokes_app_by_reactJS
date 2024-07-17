930889a965b8936375caa6d2dcc2a2d3fe3b9ccc
salaahl <sokhona.salaha@gmail.com>
2024-07-18 01:11:31 +0200

    ajustements

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import { routes } from './index';
-import { Link, useLocation, useNavigation, useOutlet } from 'react-router-dom';
+import {
+  Link,
+  useLocation,
+  useNavigate,
+  useNavigation,
+  useOutlet,
+} from 'react-router-dom';
 import { CSSTransition, SwitchTransition } from 'react-transition-group';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
@@ -9,12 +15,14 @@ import Loader from './components/Loader';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faSmile } from '@fortawesome/free-regular-svg-icons';
+import { faArrowLeftLong } from '@fortawesome/free-solid-svg-icons';
 
-library.add(faSmile);
+library.add(faArrowLeftLong, faSmile);
 
 export default function App() {
   const location = useLocation();
-  const navigation = useNavigation();
+  const navigation = useNavigation(); // Va gérer les états de navigation (parge en chargement, page chargéee...)
+  const navigate = useNavigate(); // Et lui me permettre d'aller d'une page à l'autre
   const currentOutlet = useOutlet();
   const { nodeRef } =
     routes.find((route) => route.path === location.pathname) ?? {};
@@ -22,6 +30,15 @@ export default function App() {
   return (
     <div id="App">
       <header id="App-header">
+        {location.key !== 'default' && location.pathname !== '/' ? (
+          <button onClick={() => navigate(-1)}>
+            <FontAwesomeIcon icon={faArrowLeftLong} />
+          </button>
+        ) : (
+          <button onClick={() => navigate(`/`)}>
+            <FontAwesomeIcon icon={faArrowLeftLong} />
+          </button>
+        )}
         <Link to={`/`}>
           <FontAwesomeIcon
             icon="fa-regular fa-face-smile"
